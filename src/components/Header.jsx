0c44bf82d5a828ff2624aa0af07ab4d474500ed9
NEHA <n1ehpatel20@gmail.com>
2024-02-23 12:49:29 +0530

    Font Change

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+'use client';
+import { FaTwitter } from "react-icons/fa";
+const Header = () => {
+  return (
+    <>
+        <header className="bg-[rgba(248,248,248,1)] h-[56px] w-[1440px] top-3 text-center">Free shipping on all U.S. orders $50+
+        <FaTwitter />
+        </header>
+    </>
+  )
+}
+
+export default Header
