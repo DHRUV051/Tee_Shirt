d27bcb41c2ab8c1a2b8893546aa7073c2fb78659
DHRUV051 <dhruvparmar051@gmail.com>
2024-02-26 11:20:59 +0530

    Home Container2 added

---
+++ src/Components/Home(Dhruv)/Container2.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Spotify from "../../../public/assets/Home/spotify.svg";
+import Feedly from "../../../public/assets/Home/feedly.svg";
+import Lattice from "../../../public/assets/Home/lattice.svg";
+import Hopin from "../../../public/assets/Home/hopin.svg";
+import Upwork from "../../../public/assets/Home/upwork.svg";
+
+const Container2 = () => {
+  return (
+    <main className='bg-[rgb(245,245,245)] px-[150px] py-[70px] small:px-[50px] small:py-[30px] medium:px-[50px] medium:py-[30px] flex flex-col items-center'>
+      <div className='w-2/3 small:w-full medium:w-full'>
+        <p className='text-[24px] font-bold text-center'>
+          “We've got custom T-shirts in a range of fits and sizes, so everyone can wear your brand or message.”
+        </p>
+      </div>
+      <div className="mt-[30px]">
+            <ul className="flex small:grid small:grid-cols-3 medium:grid medium:grid-cols-3">
+                <li>
+                    <Image src={Spotify} alt="" width="" height="" />
+                </li>
+                <li>
+                    <Image src={Feedly} alt="" width="" height="" />
+                </li>
+                <li>
+                    <Image src={Lattice} alt="" width="" height="" />
+                </li>
+                <li>
+                    <Image src={Hopin} alt="" width="" height="" />
+                </li>
+                <li>
+                    <Image src={Upwork} alt="" width="" height="" />
+                </li>
+                <li>
+                    <Image src={Hopin} alt="" width="" height="" />
+                </li>
+            </ul>
+      </div>
+    </main>
+  )
+}
+export default Container2
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,18 @@
-import Container1 from "@/Components/Home(Dhruv)/Container1";
+import Container1 from '@/Components/Home(Dhruv)/Container1'
+import Container2 from '@/Components/Home(Dhruv)/Container2'
+
 
 const Home = () => {
-    return (
-        <main className="px-[150px] space-x-[50px] mt-[50px] small:px-[30px] medium:px-[30px]">
-            <Container1/>
-        </main>
-    )
+  return (
+    <main className='space-y-[50px] mt-[50px] '>
+      <section className='px-[150px] small:px-[30px] medium:px-[30px]'>
+        <Container1 />
+      </section>
+      <section>
+            <Container2/>
+      </section>
+    </main>
+  )
 }
 
-export default Home;
+export default Home
